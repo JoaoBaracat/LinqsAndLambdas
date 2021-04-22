76bcd1b784a957136b337bd425998faf3d85ae56
baracat <joaodobingo@gmail.com>
2021-04-22 07:59:59 -0300

    Join e Join composto

--- LinqAndLambdas/LinqAndLambdas.App/EntryPoint.cs
+++ LinqAndLambdas/LinqAndLambdas.App/EntryPoint.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqAndLambdas.App;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -33,150 +34,53 @@ namespace LinqAndLambdas
                 new Person("John", 170, 77, Gender.Male, 26)
             };
 
-
-
-
-
-
-
-
-
-
-
-
-            var simpleGroup = people.GroupBy(p => p.Gender);
-
-            //var genderGroup = from p in people
-            //                  group p by p.Gender;
-
-            foreach (var person in simpleGroup)
+            List<Buyer> buyers = new List<Buyer>()
             {
-                Console.WriteLine($"{person.Key}");
-                foreach (var p in person)
+                new Buyer(1, "Cliente ", 1, 22),
+                new Buyer(1, "Cliente 2", 1, 22),
+                new Buyer(1, "Cliente 3", 5, 22),
+                new Buyer(1, "Cliente 4", 1, 44),
+                new Buyer(1, "Cliente 5", 1, 23),
+            };
+
+            List<Supplier> suppliers = new List<Supplier>()
+            {
+                new Supplier(1, "Fornecedor", 1, 22),
+                new Supplier(1, "Fornecedor2", 1, 22),
+                new Supplier(1, "Fornecedor3", 1, 23),
+                new Supplier(1, "Fornecedor4", 1, 31),
+            };
+
+            var innerJoin = suppliers.Join(buyers, s => s.District, b => b.District,
+                (s, b) => new
                 {
-                    Console.WriteLine($"   {p.Name} {p.Gender}");
-                }
+                    SupplierName = s.Name,
+                    BuyerName = b.Name,
+                    District = s.District
+                });
 
+
+            foreach (var item in innerJoin)
+            {
+                Console.WriteLine($"   {item.District} {item.SupplierName} {item.BuyerName}");
             }
 
-            var simpleGroupWithCondidtion = people.Where(p=> p.Age > 20)
-                                                    .GroupBy(p => p.Gender);
-            //var groupWithCondition = from p in people
-            //                         where p.Age > 20
-            //                         group p by p.Age;
 
-            foreach (var key in simpleGroupWithCondidtion)
-            {
-                Console.WriteLine($"{key.Key}");
-                foreach (var p in key)
+            var compositeJoin = suppliers.Join(buyers,
+                s => new { s.District, s.Age },
+                b => new { b.District, b.Age},
+                (s, b) => new
                 {
-                    Console.WriteLine($"   {p.Name} {p.Age}");
-                }
+                    SupplierName = s.Name,
+                    BuyerName = b.Name,
+                    District = s.District,
+                    Age = s.Age
+                });
 
-            }
-            
-            var simpleGrupoPrimeiraLetra = people.OrderBy(p=> p.Name)
-                                                    .GroupBy(p => p.Name[0]);
-            //var groupPrimeiraLetraGroup = from p in people
-            //                              orderby p.Name
-            //                         group p by p.Name[0];
 
-            foreach (var key in simpleGrupoPrimeiraLetra)
+            foreach (var item in compositeJoin)
             {
-                Console.WriteLine($"{key.Key}");
-                foreach (var p in key)
-                {
-                    Console.WriteLine($"   {p.Name}");
-                }
-
-            }
-
-            var multiKey = people
-                                .GroupBy(p => new { p.Gender, p.Age })
-                                .OrderBy(p => p.Count());
-            //var groupPrimeiraLetraGroup = from p in people
-            //                              orderby p.Name
-            //                         group p by p.Name[0];
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            foreach (var key in multiKey)
-            {
-                Console.WriteLine($"{key.Key}");
-                foreach (var p in key)
-                {
-                    Console.WriteLine($"   {p.Name} {p.Gender} {p.Age}");
-                }
-
-            }
-
-
-            warriors.ForEach(w => Console.WriteLine(w.Height));
-
-            //exemplo Join >> ta pela altura e nao por ID pq nao criei ID
-            var warriorJoinPeople = warriors
-                                    .Join(people, w => w.Height, p => p.Height, (warriors, people) => new { warriors, people })
-                                    .Select(x => new
-                                    {
-                                        x.warriors.Height,
-                                        x.people.Name,
-                                        x.people.Gender,
-                                        x.people.Weight
-                                    });
-
-
-            var shortWarrior = warriors.Select(w => w.Height); // Select returns a new collection from another collection. Cannot apply conditions on its own
-            var shortWarrior1 = warriors.Where(w => w.Height == 100); // Where returns items from the same collection that match a given condition
-
-            // Can combine those two to get where height = 100 and to return a new collection with it
-            List<int> shortwarrior2 = warriors.Where(w => w.Height == 100)
-                                        .Select(w => w.Height).ToList();
-            Console.WriteLine();
-            shortwarrior2.ForEach(sw => Console.WriteLine(sw));
-
-
-
-            // ----------------------------------------------------------------------------------------------------------------------------
-            // Lets say we want to extract all odd numbers from numbers list, and add it to a new collection. With 1 line of code, we can do that using lambda expressions.
-            List<int> oddNumbers = numbers                            
-                .Where(n => (n % 2 == 1)).ToList();
-            Console.WriteLine("OddNumbers");
-            Console.WriteLine(string.Join(", ", oddNumbers));
-
-
-            // Lets say we want to extract all integers from our mix array
-            var allIntegers = mix.OfType<int>().Where(i => i < 3);
-            Console.WriteLine(string.Join(", ", allIntegers));
-
-            var allIntLists = mix.OfType<List<int>>().ToList(); // Extracting all lists of int and then displaying them
-            for (int i = 0; i < allIntLists.Count; i++)
-            {
-                Console.WriteLine($"Int lists[{i}]: " + string.Join(", ", allIntLists[i]));
-            }
-
-
-
-            // ----------------------------------------------------------------------------------------------------
-            // LINQ Queries on Objects
-            // Lets say we want people who's name is 4 characters long, and we want them in a new collection
-            var fourCharPeople = from p in people
-                                 where (p.Name.Length == 4)
-                                 orderby p.Name descending, p.Height
-                                 select p;
-
-
-            //var nomeEAltura = fourCharPeople.Select(x => $"Name: {x.Name}, Weight: {x.Weight}");
-            //Console.WriteLine(string.Join(", ", nomeEAltura));
-
-            foreach (var item in fourCharPeople)
-            {
-                Console.WriteLine($"Name: {item.Name}, Weight: {item.Weight}");
+                Console.WriteLine($"   {item.District} {item.SupplierName} {item.BuyerName} {item.Age}");
             }
 
 
@@ -184,34 +88,176 @@ namespace LinqAndLambdas
 
 
 
-            var catsWithA = from cat in catNames
-                            where cat.Contains("a")
-                            where cat.Length < 5
-                            select cat;
-            System.Console.WriteLine(string.Join(",", catsWithA));
 
 
-            var getTheNumbers = from number in numbers
-                                where number > 5 
-                                && number < 10
-                                orderby number descending
-                                select number                                ;
+            //var simpleGroup = people.GroupBy(p => p.Gender);
 
-            //List<int> newNumbers = new List<int>();
-            //foreach(var number in numbers)
+            ////var genderGroup = from p in people
+            ////                  group p by p.Gender;
+
+            //foreach (var person in simpleGroup)
             //{
-            //    if (number < 5)
+            //    Console.WriteLine($"{person.Key}");
+            //    foreach (var p in person)
             //    {
-            //        newNumbers.Add(number);
+            //        Console.WriteLine($"   {p.Name} {p.Gender}");
             //    }
-            //}
-            System.Console.WriteLine(string.Join(",", getTheNumbers));
-            //System.Console.WriteLine(string.Join(",", newNumbers));
 
-            //1 Simple LINQ ex
-            var simpleLinq = from s in sentence
-                             select s;
-            System.Console.WriteLine(string.Join(" ", simpleLinq));
+            //}
+
+            //var simpleGroupWithCondidtion = people.Where(p=> p.Age > 20)
+            //                                        .GroupBy(p => p.Gender);
+            ////var groupWithCondition = from p in people
+            ////                         where p.Age > 20
+            ////                         group p by p.Age;
+
+            //foreach (var key in simpleGroupWithCondidtion)
+            //{
+            //    Console.WriteLine($"{key.Key}");
+            //    foreach (var p in key)
+            //    {
+            //        Console.WriteLine($"   {p.Name} {p.Age}");
+            //    }
+
+            //}
+            
+            //var simpleGrupoPrimeiraLetra = people.OrderBy(p=> p.Name)
+            //                                        .GroupBy(p => p.Name[0]);
+            ////var groupPrimeiraLetraGroup = from p in people
+            ////                              orderby p.Name
+            ////                         group p by p.Name[0];
+
+            //foreach (var key in simpleGrupoPrimeiraLetra)
+            //{
+            //    Console.WriteLine($"{key.Key}");
+            //    foreach (var p in key)
+            //    {
+            //        Console.WriteLine($"   {p.Name}");
+            //    }
+
+            //}
+
+            //var multiKey = people
+            //                    .GroupBy(p => new { p.Gender, p.Age })
+            //                    .OrderBy(p => p.Count());
+            ////var groupPrimeiraLetraGroup = from p in people
+            ////                              orderby p.Name
+            ////                         group p by p.Name[0];
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //foreach (var key in multiKey)
+            //{
+            //    Console.WriteLine($"{key.Key}");
+            //    foreach (var p in key)
+            //    {
+            //        Console.WriteLine($"   {p.Name} {p.Gender} {p.Age}");
+            //    }
+
+            //}
+
+
+            //warriors.ForEach(w => Console.WriteLine(w.Height));
+
+            ////exemplo Join >> ta pela altura e nao por ID pq nao criei ID
+            //var warriorJoinPeople = warriors
+            //                        .Join(people, w => w.Height, p => p.Height, (warriors, people) => new { warriors, people })
+            //                        .Select(x => new
+            //                        {
+            //                            x.warriors.Height,
+            //                            x.people.Name,
+            //                            x.people.Gender,
+            //                            x.people.Weight
+            //                        });
+
+
+            //var shortWarrior = warriors.Select(w => w.Height); // Select returns a new collection from another collection. Cannot apply conditions on its own
+            //var shortWarrior1 = warriors.Where(w => w.Height == 100); // Where returns items from the same collection that match a given condition
+
+            //// Can combine those two to get where height = 100 and to return a new collection with it
+            //List<int> shortwarrior2 = warriors.Where(w => w.Height == 100)
+            //                            .Select(w => w.Height).ToList();
+            //Console.WriteLine();
+            //shortwarrior2.ForEach(sw => Console.WriteLine(sw));
+
+
+
+            //// ----------------------------------------------------------------------------------------------------------------------------
+            //// Lets say we want to extract all odd numbers from numbers list, and add it to a new collection. With 1 line of code, we can do that using lambda expressions.
+            //List<int> oddNumbers = numbers                            
+            //    .Where(n => (n % 2 == 1)).ToList();
+            //Console.WriteLine("OddNumbers");
+            //Console.WriteLine(string.Join(", ", oddNumbers));
+
+
+            //// Lets say we want to extract all integers from our mix array
+            //var allIntegers = mix.OfType<int>().Where(i => i < 3);
+            //Console.WriteLine(string.Join(", ", allIntegers));
+
+            //var allIntLists = mix.OfType<List<int>>().ToList(); // Extracting all lists of int and then displaying them
+            //for (int i = 0; i < allIntLists.Count; i++)
+            //{
+            //    Console.WriteLine($"Int lists[{i}]: " + string.Join(", ", allIntLists[i]));
+            //}
+
+
+
+            //// ----------------------------------------------------------------------------------------------------
+            //// LINQ Queries on Objects
+            //// Lets say we want people who's name is 4 characters long, and we want them in a new collection
+            //var fourCharPeople = from p in people
+            //                     where (p.Name.Length == 4)
+            //                     orderby p.Name descending, p.Height
+            //                     select p;
+
+
+            ////var nomeEAltura = fourCharPeople.Select(x => $"Name: {x.Name}, Weight: {x.Weight}");
+            ////Console.WriteLine(string.Join(", ", nomeEAltura));
+
+            //foreach (var item in fourCharPeople)
+            //{
+            //    Console.WriteLine($"Name: {item.Name}, Weight: {item.Weight}");
+            //}
+
+
+
+
+
+
+            //var catsWithA = from cat in catNames
+            //                where cat.Contains("a")
+            //                where cat.Length < 5
+            //                select cat;
+            //System.Console.WriteLine(string.Join(",", catsWithA));
+
+
+            //var getTheNumbers = from number in numbers
+            //                    where number > 5 
+            //                    && number < 10
+            //                    orderby number descending
+            //                    select number                                ;
+
+            ////List<int> newNumbers = new List<int>();
+            ////foreach(var number in numbers)
+            ////{
+            ////    if (number < 5)
+            ////    {
+            ////        newNumbers.Add(number);
+            ////    }
+            ////}
+            //System.Console.WriteLine(string.Join(",", getTheNumbers));
+            ////System.Console.WriteLine(string.Join(",", newNumbers));
+
+            ////1 Simple LINQ ex
+            //var simpleLinq = from s in sentence
+            //                 select s;
+            //System.Console.WriteLine(string.Join(" ", simpleLinq));
 
 
 
---
+++ LinqAndLambdas/LinqAndLambdas.App/Supplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqAndLambdas.App
+{
+    public class Supplier
+    {
+        public Supplier(int id, string name, int district, int age)
+        {
+            Id = id;
+            Name = name;
+            District = district;
+            Age = age;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int District { get; set; }
+        public int Age { get; set; }
+    }
+}
